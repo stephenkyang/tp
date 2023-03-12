6831f6f11c0c3bd6ba4932e38e97770b668a9021
pinyoko573 <pinyoko573@gmail.com>
2023-03-13 01:33:16 +0800

    Add Deposit UI Response

--- src/main/java/seedu/duke/model/Deposit.java
+++ src/main/java/seedu/duke/model/Deposit.java
@@ -1,19 +1,19 @@
 package seedu.duke.model;
 
 public class Deposit {
-    String name;
-    double amount;
+    private String name;
+    private double amount;
 
     public Deposit(String name, double amount) {
         this.name = name;
         this.amount = amount;
     }
 
-    public String getDepositeName() {
+    public String getName() {
         return this.name;
     }
 
-    public double getDepositAmount() {
+    public double getAmount() {
         return this.amount;
     }
 }
