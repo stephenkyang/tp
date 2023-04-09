49f60f30e00e402d1c315486442515e92c337eca
chongyongrui <yrchong2000@gmail.com>
2023-04-09 23:04:36 +0800

    remove unused imports

--- src/test/java/seedu/duke/test/budget/AddBudgetTest.java
+++ src/test/java/seedu/duke/test/budget/AddBudgetTest.java
@@ -3,7 +3,6 @@ package seedu.duke.test.budget;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 
